a16c1f0609e98d96e090d5830ef71e4ca6513639
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-29 09:35:11 -0600

    REACT TS-Guitarras useReducer: Instanciando el reducer en la página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,59 @@
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
-import { useCart } from './hooks/useCart'
+import {useCart} from './hooks/useCart'
+import {useReducer} from "react";
+import {cartReducer, initialState} from "./reducers/cart-reducer.ts";
 
 function App() {
 
-  const { data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+    const {
+        data,
+        cart,
+        addToCart,
+        removeFromCart,
+        decreaseQuantity,
+        increaseQuantity,
+        clearCart,
+        isEmpty,
+        cartTotal
+    } = useCart()
+    const [state, dispatch] = useReducer(cartReducer, initialState)
+    return (
+        <>
+            <Header
+                cart={cart}
+                removeFromCart={removeFromCart}
+                decreaseQuantity={decreaseQuantity}
+                increaseQuantity={increaseQuantity}
+                clearCart={clearCart}
+                isEmpty={isEmpty}
+                cartTotal={cartTotal}
+            />
 
-  return (
-    <>
-      <Header 
-        cart={cart}
-        removeFromCart={removeFromCart}
-        decreaseQuantity={decreaseQuantity}
-        increaseQuantity={increaseQuantity}
-        clearCart={clearCart}
-        isEmpty={isEmpty}
-        cartTotal={cartTotal}
-      />
-      
-      <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestra Colección</h2>
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
 
-          <div className="row mt-5">
-              {data.map((guitar) => (
-                  <Guitar 
-                    key={guitar.id}
-                    guitar={guitar}
-                    addToCart={addToCart}
-                  />
-              ))}
-              
-          </div>
-      </main>
+                <div className="row mt-5">
+                    {data.map((guitar) => (
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
+                            addToCart={addToCart}
+                        />
+                    ))}
+
+                </div>
+            </main>
 
 
-      <footer className="bg-dark mt-5 py-5">
-          <div className="container-xl">
-              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-          </div>
-      </footer>
-    </>
-  )
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los
+                        derechos Reservados</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default App
