15aef7819b573c4687903ad8047e79bcbc49215f
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-29 10:17:58 -0600

    REACT TS-Guitarras useReducer: Migrando del Custom Hook hacia useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {cartReducer, initialState} from "./reducers/cart-reducer.ts";
 function App() {
 
     const {
-        data,
+
         cart,
         addToCart,
         removeFromCart,
@@ -34,7 +34,7 @@ function App() {
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
-                    {data.map((guitar) => (
+                    {state.data.map((guitar) => (
                         <Guitar
                             key={guitar.id}
                             guitar={guitar}
