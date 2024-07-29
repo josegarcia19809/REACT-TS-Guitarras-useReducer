d6e36d0daf6cc316a7a943fa145644309ca74ef1
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-29 10:35:34 -0600

    REACT TS-Guitarras useReducer: Ajustando el Código para la sintaxis de useReducer

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -18,32 +18,47 @@ export const initialState: CartState = {
     cart: []
 }
 
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
 export const cartReducer = (
     state: CartState = initialState,
     action: CartActions
 ) => {
-    if(action.type==="add-to-cart"){
-        return{
+    if (action.type === "add-to-cart") {
+
+        let updatedCart: CartItem[] = []
+        const itemExists = state.cart.findIndex(guitar => guitar.id === action.payload.item.id)
+        if (itemExists >= 0) { // existe en el carrito
+            if (state.cart[itemExists].quantity >= MAX_ITEMS) return
+            updatedCart = [...state.cart]
+            updatedCart[itemExists].quantity++
+        } else {
+            const newItem: CartItem = {...action.payload.item, quantity: 1}
+            updatedCart = [...state.cart, newItem];
+        }
+        return {
+            ...state,
+            cart: updatedCart
+        }
+    }
+    if (action.type === "remove-from-cart") {
+        return {
             ...state
         }
     }
-    if(action.type==="remove-from-cart"){
-        return{
+    if (action.type === "decrease-quantity") {
+        return {
             ...state
         }
     }
-    if(action.type==="decrease-quantity"){
-        return{
+    if (action.type === "increase-quantity") {
+        return {
             ...state
         }
     }
-    if(action.type==="increase-quantity"){
-        return{
-            ...state
-        }
-    }
-    if(action.type==="clear-cart"){
-        return{
+    if (action.type === "clear-cart") {
+        return {
             ...state
         }
     }
